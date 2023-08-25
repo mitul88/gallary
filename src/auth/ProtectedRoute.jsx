eb005c6ae98df5aca511a422e71cd6c16d510d1f
shahed <shahed.rahman88@gmail.com>
2023-08-25 17:50:48 +0600

    routes createdd

---
+++ src/auth/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const useAuth = () => {
+    return true
+}
+
+const ProtectedRoute = () => {
+  return 
+  (
+    <div>ProtectedRoute</div>
+  )
+}
+
+
+export default ProtectedRoute
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import{Route, Routes} from 'react-router-dom'
 import Header from './header/Header'
 import Feed from './screens/Feed'
 import ImageView from './screens/ImageView/ImageView'
@@ -7,8 +8,10 @@ const MainComponent = () => {
   return (
     <div>
       <Header />
-      <ImageView />
-      {/* <Feed /> */}
+      <Routes>
+        <Route path="/" element={<Feed />}/>
+        <Route path="/image" element={<ImageView />}/>
+      </Routes>
     </div>
   )
 }
